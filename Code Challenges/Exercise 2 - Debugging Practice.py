edc5e619cff41053c8ff6afd5b50f9c64d2e416d
Shannon Thompson <sthompson7197@gmail.com>
2023-04-15 10:15:38 -0400

    added instructions

--- Code Challenges/Exercise 2 - Debugging Practice.py
+++ Code Challenges/Exercise 2 - Debugging Practice.py
@@ -1,4 +1,8 @@
-#Fix the code below 👇
+#INSTRUCTIONS: Look at the code in the code editor on the right. There are errors in all of the lines of code. Fix the code so that it runs without errors.
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
 
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
