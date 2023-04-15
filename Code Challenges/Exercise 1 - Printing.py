9bff2df5fead7e8a8bb4cf3d0bcf6d4d3c3c115f
Shannon Thompson <sthompson7197@gmail.com>
2023-04-15 10:14:05 -0400

    added instructions

--- Code Challenges/Exercise 1 - Printing.py
+++ Code Challenges/Exercise 1 - Printing.py
@@ -1,4 +1,4 @@
-#Write your code below this line 👇
+#INSTRUCTIONS: Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function.
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
